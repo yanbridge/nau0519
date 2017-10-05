50fb766e46b098581a0acab53115ade4f2ec30f7
yanbridge <yanbridge@gmail.com>
2017-10-05 08:02:42 -0500

    Update mathfont.R

--- mathfont.R
+++ mathfont.R
@@ -17,7 +17,7 @@ ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point() +
 
 font_install("fontcm")
 loadfonts()
-png("plot_cm.pdf", family="CM Roman", width=5.5, height=5)
+pdf("plot_cm.pdf", family="CM Roman", width=5.5, height=5)
 
 curve(dnorm, from=-3, to=3, main="Normal Distribution")
 text(x=0, y=0.1, cex=1.5, expression(italic(y == frac(1, sqrt(2 * pi)) *
