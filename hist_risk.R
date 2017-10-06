a426c5797d4da384145015c4893bea2904fe8ec5
yanbridge <yanbridge@gmail.com>
2017-10-06 09:04:43 -0500

    Update hist_risk.R

--- hist_risk.R
+++ hist_risk.R
@@ -4,7 +4,7 @@ hist(hr,col="lightblue",prob=T)
 lines(density(hr), col="red", lwd=3)
 curve(dnorm(x),col='blue', lwd=3, lty=2, add=T)
 
-##change number
+## heavy tail
 hr1<-sort(hr)
 hr1[1:3]<-c(-4.5,-3.8,-4.3)
 hist(hr1,col="lightblue",prob=T)
