f027535f5397c7ad1a9e77d2897113e728832719
yanbridge <yanbridge@gmail.com>
2017-10-13 08:14:28 -0500

    Update hist_risk.R

--- hist_risk.R
+++ hist_risk.R
@@ -1,12 +1,12 @@
 set.seed(123)
 hr<-rnorm(100)
-hist(hr,col="lightblue",prob=T)
+hist(hr,col="lightblue",ylim=c(0,0.5),prob=T)
 lines(density(hr), col="red", lwd=3)
 curve(dnorm(x),col='blue', lwd=3, lty=2, add=T)
 
 ## heavy tail
 hr1<-sort(hr)
 hr1[1:3]<-c(-4.5,-3.8,-4.3)
-hist(hr1,col="lightblue",prob=T)
+hist(hr1,col="lightblue",ylim=c(0,0.45),prob=T)
 lines(density(hr1), col="red", lwd=3)
 curve(dnorm(x),col='blue', lwd=3, lty=2, add=T)
